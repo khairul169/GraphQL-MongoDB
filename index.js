b4387a8729fbf1c48cb73187e7eee408364a455d
Khairul Hidayat <contact@khairul.my.id>
2020-01-01 21:50:41 +0700

    Setup mongodb database

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 import express from 'express';
 import egql from 'express-graphql';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 import schema from './schema';
 import query from './query';
 
+// Setup dot environment
+dotenv.config();
+
+// Connect to database server
+const dbServer = process.env.DB_SERVER;
+mongoose.connect(dbServer, {useUnifiedTopology: true, useNewUrlParser: true})
+    .catch(error => console.log(error));
+
+// Database connection handler
+mongoose.connection.on('connected', () => console.log('db connected'));
+mongoose.connection.on('error', error => console.log('db error: ', error));
+
+// Configure express http server
 const app = express();
 
 app.use('/', egql({
