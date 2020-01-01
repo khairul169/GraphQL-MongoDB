8bb3922af8971c09a01a4792f6a083987c6bd7cc
Khairul Hidayat <contact@khairul.my.id>
2020-01-01 12:10:49 +0700

    Test graphql

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-const express = require('express');
+import express from 'express';
+import egql from 'express-graphql';
+import {buildSchema} from 'graphql';
+
+const app = express();
+
+const schema = buildSchema(`
+    type Query {
+        hello: String
+    }
+`);
+
+const query = {
+    hello: () => 'Hello world!'
+}
+
+app.use('/', egql({
+    graphiql: true,
+    schema,
+    rootValue: query
+}));
+
+app.listen(5050, () => {
+    console.log('Server started at port 5050.');
+})
